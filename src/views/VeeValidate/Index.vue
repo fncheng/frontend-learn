c90d8f277dd1c0311015772ddc8b17ccb76de7ad
fncheng <dong862647669@gmail.com>
2021-06-19 11:26:54 +0800

    feat: add vee-validate

---
+++ src/views/VeeValidate/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <validation-provider rules="secret" v-slot="{ errors }">
+      <input type="text" v-model="email" />
+      <span>{{ errors[0] }}</span>
+    </validation-provider>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+    }
+  },
+}
+</script>
+
+<style></style>
