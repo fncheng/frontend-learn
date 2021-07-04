cd4fd962b4f6d61429cece98b2b81a958205655c
fncheng <dong862647669@gmail.com>
2021-07-04 21:09:05 +0800

    feat: tabs动态路由切换效果
    tabs点击事件需要处理重复点击的报错问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-tabs v-model="activeName" @tab-click="tabClick">
+      <el-tab-pane
+        v-for="item in tabs"
+        :key="item.name"
+        :label="item.label"
+        :name="item.name"
+      ></el-tab-pane>
+      <router-view></router-view>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      activeName: 'first',
+      tabs: [
+        { name: 'first', label: '用户管理' },
+        { name: 'second', label: '配置管理' },
+        { name: 'third', label: '角色管理' },
+      ],
+    }
+  },
+  methods: {
+    tabClick(e) {
+      this.$router.push(e.name).catch(() => {
+        return
+      })
+    },
   },
 }
 </script>
