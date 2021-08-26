89267bd587ab7f558ad6c6861315eb9c186ca317
fncheng <dong862647669@gmail.com>
2021-08-26 20:08:24 +0800

    fix: add vue composition api

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCompositionAPI from '@vue/composition-api'
 
+Vue.use(VueCompositionAPI)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
