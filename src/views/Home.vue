0c2120798a839a4827c9f8627b4b413c24d70245
fncheng <dong862647669@gmail.com>
2021-08-26 20:10:27 +0800

    fix: postcss-px-to-viewport

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
-    <div>
-      1
-      <!-- 123 -->
-    </div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -13,8 +14,40 @@
 export default {
   name: 'Home',
   components: {},
-  created() {
-    console.log('created')
+  setup() {
+    const tableData = [
+      {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+      },
+      {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+      },
+      {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+      },
+      {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+      },
+    ]
+    return { tableData }
   },
 }
 </script>
+
+<style lang="scss" scoped>
+// .home {
+//   width: 100%;
+//   height: 192px;
+// }
+.home {
+  height: 600px;
+}
+</style>
