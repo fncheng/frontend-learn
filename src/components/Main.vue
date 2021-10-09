d55983e28947c52bcf2fafdeb642576b69032e16
fncheng <dong862647669@gmail.com>
2021-10-09 18:03:15 +0800

    fix: jsx elementui

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,7 @@
 <script>
 import Mouse from './Mouse'
 import Cat from './Cat'
+import { ref } from '@vue/composition-api'
 export default {
   name: 'Main',
   methods: {
@@ -8,12 +9,27 @@ export default {
       console.log(123)
     },
   },
+  setup() {
+    const name = ref('name')
+    const doInput = (e) => {
+      name.value = e
+    }
+    return {
+      name,
+      doInput,
+    }
+  },
   render() {
+    const Dog = () => <h3>{this.name}</h3>
     return (
       <div>
+        <Dog></Dog>
         <h1>移动鼠标！</h1>
         {this.$attrs.myName}
-        <Mouse render={(mouse) => <Cat mouse={mouse} />}></Mouse>
+        <Mouse
+          render={(mouse) => <Cat mouse={mouse} />}
+          ondo-input={this.doInput}
+        ></Mouse>
       </div>
     )
   },
--- src/components/Mouse.vue
+++ src/components/Mouse.vue
@@ -8,7 +8,7 @@ export default {
       name: 'zs',
     }
   },
-  setup() {
+  setup(props, ctx) {
     const state = reactive({
       x: 0,
       y: 0,
@@ -17,17 +17,65 @@ export default {
       state.x = e.clientX
       state.y = e.clientY
     }, 300)
+    const handleInput = (e) => {
+      ctx.emit('do-input', e)
+    }
     return {
       debounce,
       state,
       doMouseMove,
+      handleInput,
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
     }
   },
   render() {
     return (
       <div style="height: 100vh" onMousemove={this.doMouseMove}>
         {this.$attrs.render(this.state)}
-        <el-input vModel={this.name} class="input"></el-input>
+        <el-input
+          vModel={this.name}
+          class="input"
+          onInput={this.handleInput}
+        ></el-input>
+        <el-table
+          data={this.tableData}
+          style="width:500px"
+          cell-style={{ textAlign: 'center' }}
+        >
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="100"
+          ></el-table-column>
+          <el-table-column prop="date" label="日期"></el-table-column>
+          <el-table-column
+            prop="address"
+            label="住址"
+            width="100"
+            align="left"
+          ></el-table-column>
+        </el-table>
       </div>
     )
   },
