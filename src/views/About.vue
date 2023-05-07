ee930948fb4679b01db0316520beab85c4170ca8
ASUS\cheng <dong862647669@gmail.com>
2023-05-07 18:10:12 +0800

    feat: websocket连接测试

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,27 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  mounted() {
+    console.log('About')
+
+    if (window.WebSocket) {
+      console.log('webscoket服务顺利运行!')
+      const ws = new WebSocket('ws://localhost:3000/socketTest')
+      ws.onopen = (e) => {
+        console.log('onopen')
+        ws.send('Hi', e)
+      }
+      ws.onmessage = (e) => {
+        console.log('Message from', e)
+      }
+      ws.onerror = (e) => {
+        console.log('连接失败', e)
+      }
+    }
+  },
+}
+</script>
