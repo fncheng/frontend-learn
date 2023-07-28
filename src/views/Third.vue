bfe95a7556e11dc86cbb0c1b360ef1becb50d1ee
fncheng <dong862647669@gmail.com>
2023-07-29 02:03:20 +0800

    feat: Form表单校验动态prop

--- src/views/Third.vue
+++ src/views/Third.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="w-[600px] bg-red-300">
-    <el-form :model="ruleForm" :rules="rules">
-      <el-form-item label="姓名" prop="name">
-        <el-input></el-input>
-      </el-form-item>
-      <el-form-item label="年龄" prop="age">
-        <el-input></el-input>
-      </el-form-item>
+  <div class="flex flex-col justify-center items-center">
+    <el-form
+      style="width: 600px; margin-top: 20px"
+      :model="ruleForm"
+      :rules="rules"
+      label-position="left"
+      label-width="auto"
+    >
+      <div v-for="(item, index) in ruleForm.formdata" :key="index">
+        <el-form-item
+          label="用户姓名"
+          :prop="`formdata.${index}.name`"
+          :rules="rules.name"
+        >
+          <el-input v-model="item.name"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="年龄"
+          :prop="`formdata.${index}.age`"
+          :rules="rules.age"
+        >
+          <el-input v-model.number="item.age"></el-input>
+        </el-form-item>
+      </div>
+      <el-button @click="addFormItem">addFormItem</el-button>
     </el-form>
   </div>
 </template>
@@ -17,14 +34,31 @@ export default {
   data() {
     return {
       ruleForm: {
-        name: '',
-        age: 0,
+        formdata: [{ name: '', age: '' }],
+      },
+      rules: {
+        name: [{ required: true, message: 'name is required!' }],
+        age: [
+          { required: true, message: 'age is required!' },
+          {
+            validator: (rule, value, callback) => {
+              console.log(value)
+              if (value) {
+                //
+              }
+              callback()
+            },
+          },
+        ],
       },
     }
   },
   methods: {
-    handleClick() {
-      this.count++
+    addFormItem() {
+      this.ruleForm.formdata.push({
+        name: '',
+        age: 100,
+      })
     },
     handleClose() {
       this.$destroy()
